8c425370b8a4b36cda9a924eaed096eba8a5333a
a-ke <642028114@qq.com>
2020-08-23 20:04:57 +0800

    vuex的第一个demo小测试

---
+++ src/components/Addition.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h3>当前最新的count值为：{{ $store.state.count }}</h3>
+    <h2>{{ $store.getters.showNum }}</h2>
+    <button @click="btnHandler1">+1</button>
+    <button @click="btnHandler2">+N</button>
+    <button @click="btnHandler3">+1 Async</button>
+    <button @click="btnHandler4">+N Async</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    btnHandler1() {
+      this.$store.commit('add')
+    },
+    btnHandler2() {
+      this.$store.commit('add', 5)
+    },
+    btnHandler3() {
+      this.$store.dispatch('addAsync')
+    },
+    btnHandler4() {
+      this.$store.dispatch('addNAsync', 5)
+    }
+  }
+}
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,52 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    count: 0
   },
+  // 只有 mutations 中定义的函数，才有权利修改 state 中的数据
   mutations: {
+    add (state) {
+      state.count++
+    },
+    addN (state, step) {
+      state.count += step
+    },
+    sub (state) {
+      state.count--
+    },
+    subN (state, step) {
+      state.count -= step
+    }
   },
   actions: {
+    addAsync (context) {
+      setTimeout(() => {
+        // 在 actions 中，不能直接修改 state 中的数据；
+        // 必须通过 context.commit() 触发某个 mutation 才行
+        context.commit('add')
+      }, 2000)
+    },
+    addNAsync (context, step) {
+      setTimeout(() => {
+        context.commit('addN', step)
+      }, 2000)
+    },
+    subAsync (context) {
+      setTimeout(() => {
+        context.commit('sub')
+      }, 2000)
+    },
+    subNAsync (context, step) {
+      setTimeout(() => {
+        context.commit('subN', step)
+      }, 2000)
+    }
   },
   modules: {
+  },
+  getters: {
+    showNum(state) {
+      return `当前最新的数量是【 ${state.count} 】`
+    }
   }
 })
